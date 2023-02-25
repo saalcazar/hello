daab363b6c98f9d6be4a655a3ec704f73945dbf7
saalcazar <alcazar.samuel@gmail.com>
2023-02-25 01:33:23 -0400

    Segundo commit

--- greet/greet.go
+++ greet/greet.go
@@ -1,23 +1,10 @@
-// el paquete greet provee la funcionalidad
-
-// de saludar en diferentes lenguajes
-
 package greet
-
-// emoji es una variable a nivel paquete, es no exportable
 
 var emoji = "🙋"
 
-// English retorna saludo en inglés, es exporable
-
 func English() string {
-
 	return "Hi" + emoji
-
 }
-
-// Italian retorn saludo en italiano, es exportable
-
 func Italian() string {
 
 	return "Ciao" + emoji
